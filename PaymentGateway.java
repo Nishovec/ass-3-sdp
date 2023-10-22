54c01aabd7fc252d454bf7c0ccf63a326311595d
Nishovec <shaymardanov_E@inbox.ru>
2023-10-22 22:43:08 +0600

    1

---
+++ PaymentGateway.java
@@ -0,0 +1,5 @@
+public class PaymentGateway {
+    public boolean processPayment(double amount) {
+        return true;
+    }
+}
---
+++ TicketProvider.java
@@ -0,0 +1,5 @@
+public class TicketProvider {
+    public boolean buyTicket(String eventname, int quantity) {
+        return true;
+    }
+}
